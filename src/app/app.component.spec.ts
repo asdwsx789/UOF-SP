31dca2899950ce5e0d3b0a33e43ccf63beda94a5
TW-1130-黃裕善 <u10622@umec.com.tw>
2024-07-02 14:49:39 +0800

    first commit.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'uof-sc'`, () => {
+  it(`should have as title 'UOF-SP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('uof-sc');
+    expect(app.title).toEqual('UOF-SP');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('uof-sc app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('UOF-SP app is running!');
   });
 });
